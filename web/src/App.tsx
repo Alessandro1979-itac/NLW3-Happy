0eedddb7efc1269a0c346cb2d48414e42e68251a
Alessandro <sandromuniz27@hotmail.com>
2020-10-21 16:45:48 -0400

    Finish the web application

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './styles/global.css';
+import Routes from './routes';
+
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Routes />
   );
 }
 
